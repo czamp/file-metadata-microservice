7d70b4becf8a5c7558c0649b1574941691d08fb7
czamp <clay.zamperini@gmail.com>
2019-05-15 11:46:33 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
+
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  console.log('File Uploaded')
+  res.json({
+    name: req.file.originalname,
+    size: req.file.size,
+    type: req.file.mimetype
+  })
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
+
